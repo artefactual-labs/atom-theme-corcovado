f94d63747c60b5475b4634d30bff6ef2ccf5bdfb
Jesús García Crespo <jesus@sevein.com>
2016-01-22 14:27:36 -0800

    Basic skeleton

---
+++ config/arCorcovadoPluginConfiguration.class.php
@@ -0,0 +1,27 @@
+<?php
+
+   class arCorcovadoPluginConfiguration extends sfPluginConfiguration
+   {
+     public static
+       $summary = 'Theme plugin, extension of arCorcovadoPlugin.',
+       $version = '0.0.1';
+
+     public function contextLoadFactories(sfEvent $event)
+     {
+       $context = $event->getSubject();
+       $context->response->addStylesheet('/plugins/arCorcovadoPlugin/css/min.css', 'last', array('media' => 'all'));
+     }
+
+     public function initialize()
+     {
+       $this->dispatcher->connect('context.load_factories', array($this, 'contextLoadFactories'));
+
+       $decoratorDirs = sfConfig::get('sf_decorator_dirs');
+       $decoratorDirs[] = $this->rootDir.'/templates';
+       sfConfig::set('sf_decorator_dirs', $decoratorDirs);
+
+       $moduleDirs = sfConfig::get('sf_module_dirs');
+       $moduleDirs[$this->rootDir.'/modules'] = false;
+       sfConfig::set('sf_module_dirs', $moduleDirs);
+     }
+   }
